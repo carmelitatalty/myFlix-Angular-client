ab386a6c8fcd218ead74debffbb7c8d0ab1dc52b
carmelitatalty <94077977+carmelitatalty@users.noreply.github.com>
2024-01-24 11:09:12 -0800

    Added registration and login

---
+++ src/app/user-login-form/user-login-form.component.ts
@@ -0,0 +1,66 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+// You'll use this import to close the dialog on success
+import { MatDialogRef } from '@angular/material/dialog';
+
+// This import brings in the API calls we created in 6.2
+import { FetchApiDataService } from '../fetch-api-data.service';
+
+// This import is used to display notifications back to the user
+import { MatSnackBar } from '@angular/material/snack-bar';
+import {
+  MatCard,
+  MatCardContent,
+  MatCardHeader,
+  MatCardTitle,
+  MatCardActions,
+} from '@angular/material/card';
+import { MatFormField } from '@angular/material/form-field';
+import { FormsModule } from '@angular/forms';
+@Component({
+  selector: 'app-user-login-form',
+  standalone: true,
+  imports: [
+    MatCard,
+    MatCardHeader,
+    MatCardTitle,
+    MatCardContent,
+    MatFormField,
+    MatCardActions,
+    FormsModule,
+  ],
+  templateUrl: './user-login-form.component.html',
+  styleUrl: './user-login-form.component.scss',
+})
+export class UserLoginFormComponent implements OnInit {
+  @Input() userData = { Username: '', Password: '' };
+
+  constructor(
+    public fetchApiData: FetchApiDataService,
+    public dialogRef: MatDialogRef<UserLoginFormComponent>,
+    public snackBar: MatSnackBar
+  ) {}
+
+  ngOnInit(): void {}
+
+  // This is the function responsible for sending the form inputs to the backend
+  loginUser(): void {
+    this.fetchApiData.userLogin(this.userData).subscribe(
+      (result) => {
+        console.log(JSON.stringify(result));
+        localStorage.setItem('token', result.token);
+        localStorage.setItem('user', JSON.stringify(result.user));
+        // Logic for a successful user registration goes here! (To be implemented)
+        this.dialogRef.close(); // This will close the modal on success!
+        this.snackBar.open(result, 'OK', {
+          duration: 2000,
+        });      },
+      (result) => {
+        console.log(JSON.stringify(result));
+        this.snackBar.open(result, 'OK', {
+          duration: 2000,
+        });
+      }
+    );
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,20 @@ import { AppComponent } from './app/app.component';
 
 import { HttpClientModule } from '@angular/common/http';
 import { importProvidersFrom } from '@angular/core';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { FormsModule } from '@angular/forms';
+import { NgModule } from '@angular/core';
 
 bootstrapApplication(AppComponent, {
   ...appConfig,
-  providers: [importProvidersFrom(HttpClientModule)],
+  providers: [
+    importProvidersFrom(HttpClientModule),
+    provideAnimations(),
+    importProvidersFrom(MatCardModule),
+    importProvidersFrom(MatFormFieldModule),
+    importProvidersFrom(FormsModule),
+    importProvidersFrom(NgModule),
+  ],
 }).catch((err) => console.error(err));
